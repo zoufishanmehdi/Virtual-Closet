2c2913ad681435b90c0aac872a3797596ff74040
Zouf <zzmehdi@gmail.com>
2019-03-23 15:06:08 -0400

    added input container view

--- Virtual Closet/LoginController.swift
+++ Virtual Closet/LoginController.swift
@@ -14,7 +14,16 @@ class LoginController: UIViewController {
         super.viewDidLoad()
         
         view.backgroundColor = UIColor(red:1.00, green:0.35, blue:0.38, alpha:1.0)
-
+        
+        let inputsContainerView = UIView()
+        inputsContainerView.backgroundColor = .white
+        inputsContainerView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(inputsContainerView)
+        
+        inputsContainerView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        inputsContainerView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        inputsContainerView.widthAnchor.constraint(equalTo: view.widthAnchor, constant: -24).isActive = true
+        inputsContainerView.heightAnchor.constraint(equalToConstant: 150).isActive = true
     }
 
     
