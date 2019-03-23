c61121200793a9c3700a4b8ab8a6d689a7657f5d
Zouf <zzmehdi@gmail.com>
2019-03-23 15:21:58 -0400

    register button is displayed

--- Virtual Closet/LoginController.swift
+++ Virtual Closet/LoginController.swift
@@ -9,22 +9,48 @@
 import UIKit
 
 class LoginController: UIViewController {
-
+    
+    let inputsContainerView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .white
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.layer.cornerRadius = 5
+        view.layer.masksToBounds = true
+        return view
+    }()
+    
+    let loginRegisterButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.backgroundColor = UIColor(red:1.00, green:0.35, blue:0.38, alpha:1.0)
+        button.setTitle("Register", for: .normal)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        view.backgroundColor = UIColor(red:1.00, green:0.35, blue:0.38, alpha:1.0)
+        view.backgroundColor = UIColor(red:1.00, green:0.35, blue:0.38, alpha:0.7)
         
-        let inputsContainerView = UIView()
-        inputsContainerView.backgroundColor = .white
-        inputsContainerView.translatesAutoresizingMaskIntoConstraints = false
         view.addSubview(inputsContainerView)
+        view.addSubview(loginRegisterButton)
         
+        setupInputsContainerView()
+        
+        setupLoginRegisterButton()
+    }
+    
+    func setupInputsContainerView() {
         inputsContainerView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
         inputsContainerView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
         inputsContainerView.widthAnchor.constraint(equalTo: view.widthAnchor, constant: -24).isActive = true
         inputsContainerView.heightAnchor.constraint(equalToConstant: 150).isActive = true
     }
-
     
+    func setupLoginRegisterButton() {
+        loginRegisterButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        loginRegisterButton.topAnchor.constraint(equalTo: inputsContainerView.bottomAnchor, constant: 12).isActive = true
+        loginRegisterButton.widthAnchor.constraint(equalTo: inputsContainerView.widthAnchor).isActive = true
+        loginRegisterButton.heightAnchor.constraint(equalToConstant: 30).isActive = true 
+    }
 }
